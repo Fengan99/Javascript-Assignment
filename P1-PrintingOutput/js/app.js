087c0867b54814c11be5b8c6807178862531ea6c
Fengan99 <kenfen163@ihmeducation.se>
2024-09-12 11:11:34 +0200

    Create P4variables

--- P1-PrintingOutput/js/app.js
+++ P1-PrintingOutput/js/app.js
@@ -1,3 +1,16 @@
-console.log("Hello World!");
-console.log("My name is ben");
+let hoursofsleep = 10;
+let hoursInClass = 5;
+console.log(hoursInClass);
+console.log(hoursofsleep);
+
+const five = 5;
+const eight = 8;
+let total = 0;
+
+console.log (five);
+console.log(eight);
+console.log(five + eight);
+total = five + eight
+// 5+ 8 = 13
+
 
