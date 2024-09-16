a928a77e0f0a0441289aedd4dd2d160445c61940
Fengan99 <kenfen163@ihmeducation.se>
2024-09-16 11:53:18 +0200

    IF SATS-ELSE /BOOLEAN

---
+++ P7-Boolean/js/app.js
@@ -0,0 +1,42 @@
+//const birthYear = 1999;
+//const currentyear = 2024;
+//let ageOfPerson = currentyear -birthYear;
+//6
+//let isChild = ageOfPerson <= 11;
+
+//let isTeenager =  ageOfPerson >= 11 && ageOfPerson <=17        ;
+
+//let isAdult =ageOfPerson>= 18;
+
+//console.log(isChild);
+//console.log(isTeenager);
+//console.log(isAdult);
+
+//console.log("personen heter fredrik feng och är " + ageOfPerson);
+//console.log("personen är " +ageOfPerson +" gammal" + " och är född i Sverige");
+
+IF OCH IF SATS / BOOLEAN
+
+
+
+//let playerTouchGround = false;
+
+//let playerHasJetPak = false;
+
+//if (playerTouchGround) {
+  //Runs when playertouchground = true;
+ // console.log("player is on the ground !");
+  //console.log("Player can Jump!");
+//}
+
+
+//else if (playerHasJetPak) {
+  //runs when playerhasjetpack = true;
+  //console.log("Player can Jet Away");
+//}
+
+//else {
+  //Runs when playertouchground = false && player hasjetpack = false
+  //console.log("Player is not on the ground ");
+//console.log("player CANNOT JUMP!");
+//}
