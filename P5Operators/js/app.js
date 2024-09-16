1fd948ec770da8caa2d3ee42b185d102d9cd68c7
Fengan99 <kenfen163@ihmeducation.se>
2024-09-16 10:14:45 +0200

    P5 operators

---
+++ P5Operators/js/app.js
@@ -0,0 +1,48 @@
+//let speedinput = 72;
+//let speedOutput = 0;
+//speedOutput=speedinput /3.6;
+//console.log(speedOutput);
+
+//let minutesinput = 3;
+//let secoundoutput = 0;
+//... 180 (60*3)
+//console.log(secoundoutput);
+
+//et num1 = 11;
+//let num2 = 4;
+
+//console.log(num1 / num2);
+
+//let num3 = 11;
+//let num4 = 4;
+//console.log(num3 % num4);
+
+//let radius = 2
+//let area = 3.14
+//console.log(radius*radius*area);
+
+//let num1 = 11
+//let num2 = -1
+//console.log(num1*num2);
+
+//let vikt = 70;
+//let langd = 1.82;
+//let langd2 = 1.82*1.82;
+//console.log(vikt/langd2);
+
+//let sid1 = 3
+//let sid2= 4
+
+//let hypotenuse;
+
+//C^2 = a^2*b^2
+
+//hypotenuse = Math.pow(sid1, y = 3) + Math.pow(sid2,y =2);
+
+//console.log(hypotenuse);
+
+let numberOfBottles = 10;
+const line1 = "green bottles hanging on the wall";
+const line2 = "and if one green bottle, should accidentally fall, there will be";
+
+console.log(numberOfBottles);
---
+++ P5Operators/js/app22.js
@@ -0,0 +1,25 @@
+//let x = 5 + 5; // x = 10
+//let y = x+5; // y + 15
+//let z = y+y; //
+//console.log(z+z); //60
+
+//let firstnumber = 1;
+//let secondnumber = 2;
+//let seven = 7;
+// firstnumber + secondnumber + seven = 10
+
+//answer = firstnumber + secondnumber;
+//console.log(answer);
+
+//answer = firstnumber - secondnumber;
+//console.log(answer);
+
+//answer = firstnumber * secondnumber;
+//console.log(answer);
+
+//answer= firstnumber / secondnumber;
+//console.log(answer);
+
+//answer = firstnumber % secondnumber;
+//console.log(answer);
+
