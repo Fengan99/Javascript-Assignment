a0e2b8d588bbbd92db0f7cd5ea346dc61e9ed0ba
Fengan99 <kenfen163@ihmeducation.se>
2024-09-16 14:58:24 +0200

    P9 functions

---
+++ P9-Functions/js/app.js
@@ -0,0 +1,35 @@
+console.log("move forward");
+console.log("turn right");
+
+function stepForward() {
+  console.log("Move forward");
+}
+
+function turnright() {
+  console.log("Turn right");
+}
+
+function turnleft() {
+  console.log("turn right");
+  console.log("turn right");
+  console.log("turn right");
+
+}
+turnright()
+
+function Turnleft(){
+  console.log("turn right");
+  console.log("turn right ");
+  console.log("turn right");
+}
+
+turnleft();
+
+function walkthreesteps (){
+  console.log("move forward");
+  console.log("move forward");
+  console.log("move forward");
+
+}
+
+walkthreesteps()
