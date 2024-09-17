0cf801032d77a4574a9e4fd99aba94d385b1c662
Fengan99 <kenfen163@ihmeducation.se>
2024-09-17 09:44:17 +0200

    P4variables11

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,9 +1,13 @@
-let numberOfBottles = 10;
-const line1 = " green bottles hanging on the wall ";
-const line2 = " and if one green bottle, should accidental fall, there will be "
+//let numberOfBottles = 10;
+//const line1 = " green bottles hanging on the wall ";
+//const line2 = " and if one green bottle, should accidental fall, there will be "
 
-console.log( line1+ line2+ numberOfBottles + line1);
-console.log(line2 + --numberOfBottles+line1);
+//console.log( line1+ line2+ numberOfBottles + line1);
+//console.log(line2 + --numberOfBottles+line1);
+
+let isOfFullAge = true;
+let hasEnoughMoney = true;
+let mayBuyCar = isOfFullAge && hasEnoughMoney;
 
 
 
